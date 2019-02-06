1887070aa2b32482002985696a73a21bc4abddff
JamesPriest <jamesopriest@gmail.com>
2019-02-07 00:19:21 +1100

    Part 2 Day 1 Year 2015

--- 2015/1/solution.py
+++ 2015/1/solution.py
@@ -6,6 +6,17 @@ def count(inp):
     down = total[')']
     return up - down
 
+def negative_floor(inp):
+    floor_count = 0
+
+    for idx, char in enumerate(inp):
+        if char == '(':
+            floor_count += 1
+        elif char == ')':
+            floor_count -= 1
+        if floor_count < 0:
+            return idx + 1
+
 def test_input():
     with open('tests.txt') as file:
         for line in file:
@@ -18,3 +29,4 @@ if __name__ == '__main__':
     with open('input.txt') as file:
         data = file.read().strip()
         print(f"Solution 1: {count(data)}")
+        print(f"Solution 2: {negative_floor(data)}")
